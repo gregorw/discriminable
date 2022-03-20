1471e9f27804086b4293d2af0f683292c5628d2f
Gregor Wassmann <gregor.wassmann@gmail.com>
2022-03-20 09:44:25 +0100

    Teardown test cases

--- test/test_discriminable.rb
+++ test/test_discriminable.rb
@@ -4,6 +4,10 @@ require "test_helper"
 require "setup_orders"
 
 class TestDiscriminable < Minitest::Test
+  def teardown
+    Order.delete_all
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::Discriminable::VERSION
   end
@@ -12,5 +16,11 @@ class TestDiscriminable < Minitest::Test
     Order.create! state: :completed
     Cart.create!
     assert_equal 1, Cart.count
+  end
+
+  def test_scopes
+    Order.create! state: :completed
+    Cart.create!
+    assert_equal 1, Order.open.count
   end
 end
